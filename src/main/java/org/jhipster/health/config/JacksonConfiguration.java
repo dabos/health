0d9923e57a55fe5684c675267cfa2e767d9b63b3
alex.bostan <alex.bostan@hostelworld.com>
2015-12-04 12:01:43 +0000

    Initial checkin

---
+++ src/main/java/org/jhipster/health/config/JacksonConfiguration.java
@@ -0,0 +1,26 @@
+package org.jhipster.health.config;
+
+import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.jhipster.health.domain.util.CustomDateTimeDeserializer;
+import org.jhipster.health.domain.util.CustomDateTimeSerializer;
+import org.jhipster.health.domain.util.CustomLocalDateSerializer;
+import org.jhipster.health.domain.util.ISO8601LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.joda.JodaModule;
+
+@Configuration
+public class JacksonConfiguration {
+
+    @Bean
+    public JodaModule jacksonJodaModule() {
+        JodaModule module = new JodaModule();
+        module.addSerializer(DateTime.class, new CustomDateTimeSerializer());
+        module.addDeserializer(DateTime.class, new CustomDateTimeDeserializer());
+        module.addSerializer(LocalDate.class, new CustomLocalDateSerializer());
+        module.addDeserializer(LocalDate.class, new ISO8601LocalDateDeserializer());
+        return module;
+    }
+}
